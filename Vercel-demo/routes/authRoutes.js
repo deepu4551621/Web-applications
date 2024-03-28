0cf138b023d37eedb97152ff2983a018883db5ee
deepu4551621 <77355959+deepu4551621@users.noreply.github.com>
2024-03-28 19:20:33 +0530

    new changes

--- Vercel-demo/routes/authRoutes.js
+++ Vercel-demo/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require('cors');
 const  {Login}  = require('./controllers/authControles')
 const authenticateUser  = require('../middlewares/authMiddleware')
-const allowOrigins=['https://swiftshopper-b78ae.web.app',
+const allowOrigins=['https://swiftshopper-b78ae.web.app/',
 'http://localhost:3000',
 'https://swiftshopper-b78ae.firebaseapp.com']
 router.use(cors({
@@ -15,7 +15,4 @@ router.post('/login', Login);
 router.get('/',authenticateUser, (req, res)=>{
     res.json({message:"user authenticated successfully"})
 } );
-// router.get('/',(req,res)=>{
-//     res.send("<h1>backend working </h1>")
-// })
 module.exports = router;
